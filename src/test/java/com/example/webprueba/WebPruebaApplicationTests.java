3e9b44d99212d68f70a800bacf62555d2b5988a9
jefrires2000 <jefrires2000@gmail.com>
2024-04-13 22:19:45 -0500

    first commit

---
+++ src/test/java/com/example/webprueba/WebPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
